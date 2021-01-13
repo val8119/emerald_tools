328f7b1554e1d0342d585a8a906a4babb7188746
val8119 <48407846+val8119@users.noreply.github.com>
2021-01-13 16:42:18 +0200

    Added Emerald Nugget item and texture

--- src/main/java/val8119/emerald/tools/EmeraldTools.java
+++ src/main/java/val8119/emerald/tools/EmeraldTools.java
@@ -1,14 +1,20 @@
 package val8119.emerald.tools;
 
+import net.minecraft.util.registry.*;
+
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
 
 public class EmeraldTools implements ModInitializer {
+
+	public static final Item EMERALD_NUGGET = new Item(new Item.Settings().group(ItemGroup.MATERIALS));
+
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
 
-		System.out.println("Hello Fabric world!");
+		Registry.register(Registry.ITEM, new Identifier("emeraldtools", "emerald_nugget"), EMERALD_NUGGET);
+
 	}
 }
