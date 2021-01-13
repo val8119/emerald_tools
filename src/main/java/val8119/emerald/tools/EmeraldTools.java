e3c5a1c78e3cebb61794d987ac2745889c2fd774
val8119 <48407846+val8119@users.noreply.github.com>
2021-01-13 16:18:50 +0200

    Update EmeraldTools.java

--- src/main/java/val8119/emerald/tools/EmeraldTools.java
+++ src/main/java/val8119/emerald/tools/EmeraldTools.java
@@ -1,8 +1,8 @@
-package net.fabricmc.example;
+package val8119.emerald.tools;
 
 import net.fabricmc.api.ModInitializer;
 
-public class ExampleMod implements ModInitializer {
+public class EmeraldTools implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
